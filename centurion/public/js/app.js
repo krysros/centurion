3884912b8f22374d925979ea4d6b17a5b1afdd9b
krysros <krystian.rosinski@gmail.com>
2025-10-18 22:40:31 +0200

    Remove app.js

--- centurion/public/js/app.js
+++
@@ -1,4 +0,0 @@
-// Intentionally minimal: autocomplete is powered by htmx + datalist fragments
-// served from the server at /autocomplete. No jQuery dependency.
-
-// Keep this file available in case small vanilla helpers are needed later.
